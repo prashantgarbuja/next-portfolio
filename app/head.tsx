cb77eb8f79df593bea968f6b185db8e9e0bd09a9
Prashant Garbuja <prashantmaha@yahoo.com>
2023-10-25 05:53:05 +1100

    Updated with my personalized meta data.

--- app/head.tsx
+++ app/head.tsx
@@ -2,23 +2,43 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta name="keywords" content="jigar sable, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
-      <meta property="og:site_name" content="Jigar Sable's Portfolio" />
-      <meta property="og:title" content="Portfolio | Jigar Sable - Full Stack Developer" />
-      <meta property="og:description"
-        content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta property="og:url" content="https://jigarsable.vercel.app" />
-      <meta property="og:image" content="https://jigarsable.vercel.app/portfolio-fork.png" />
-      <link rel="canonical" href="https://jigarsable.vercel.app" />
-      <meta name="author" content="Jigar Sable" />
+      <meta
+        name="description"
+        content="I'm Prashant Garbuja, Java developer based in Sydney, Australia. Java is my love and I love to hunt bugs."
+      />
+      <meta
+        name="keywords"
+        content="Prashant Garbuja, portfolio, prashant, java dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio"
+      />
+      <meta property="og:site_name" content="Prashant Garbuja's Portfolio" />
+      <meta
+        property="og:title"
+        content="Portfolio | Prashant Garbuja - Full Stack Developer"
+      />
+      <meta
+        property="og:description"
+        content="I'm Prashant Garbuja, java developer based in Sydney, Australia. Java is my love and I love to hunt bugs."
+      />
+      <meta property="og:url" content="https://prashantgarbuja.com" />
+      <meta
+        property="og:image"
+        content="https://jigarsable.vercel.app/portfolio-fork.png"
+      />
+      <link rel="canonical" href="https://prashantgarbuja.com" />
+      <meta name="author" content="Prashant Garbuja" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://jigarsable.vercel.app" />
-      <meta name="application-name" content="Portfolio | Jigar Sable - Full Stack Developer" />
-      <meta name="apple-mobile-web-app-title" content="Portfolio | Jigar Sable - Full Stack Developer" />
-      <link rel="icon" href="/favicon.ico" />
-      <title>Portfolio | Jigar Sable - Full Stack Developer</title>
+      <meta name="twitter:site" content="https://prashantgarbuja.com" />
+      <meta
+        name="application-name"
+        content="Portfolio | Prashant Garbuja - Java Developer"
+      />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="Portfolio | Prashant Garbuja - Java Developer"
+      />
+      <link rel="icon" href="/ava.png" />
+      <title>Portfolio | Prashant Garbuja - Java Developer</title>
     </>
-  )
+  );
 }
